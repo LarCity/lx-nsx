b17946d281d1679482dab232afab7f0a0bd2db34
Uchenna Chilaka <uche@lar.city>
2025-11-22 04:27:19 -0500

    savepoint

---
+++ lib/lx/nsx.rb
@@ -0,0 +1,8 @@
+require "lx/nsx/version"
+
+module Lx
+  module Nsx
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/lx/nsx/version.rb
@@ -0,0 +1,5 @@
+module Lx
+  module Nsx
+    VERSION = "0.1.0"
+  end
+end
